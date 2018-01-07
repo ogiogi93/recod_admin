fa641d654cbf1d07d166ab249bab01a371c31afc
ogiogi93 <mail_ogiwara93@dx5.so-net.ne.jp>
2018-01-07 16:12:09 +0900

    staticfileのpathを設定

--- recod_admin/settings.py
+++ recod_admin/settings.py
@@ -115,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/recod_admin/staticfiles'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
