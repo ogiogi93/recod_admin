a1e89cc1e7303783db6a9cb4ecf7f528ee068efa
ogiogi93 <mail_ogiwara93@dx5.so-net.ne.jp>
2018-01-20 12:59:32 +0900

    modelを定義

---
+++ competition/models.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+
+
+class Competition(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, null=False)
+    logo_url = models.ImageField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(max_length=1024)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'competitions'
+        managed = True
+
+
+class Schedule(models.Model):
+    id = models.AutoField(primary_key=True)
+    competition = models.ForeignKey(Competition, on_delete=False)
+    name = models.CharField(max_length=255, null=False)
+    start_datetime = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'schedules'
+        managed = True
