3b77f2b81e2d4a6a5e8c12a424bbddffe6e541d7
ogiogi93 <mail_ogiwara93@dx5.so-net.ne.jp>
2018-02-25 20:35:31 +0900

    path変更

--- recod_admin/storage_backends.py
+++ recod_admin/storage_backends.py
@@ -1,8 +1,6 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
-from recod_admin.settings import AWS_LOCATION
-
 
 class MediaStorage(S3Boto3Storage):
-    location = AWS_LOCATION + '/media'
+    location = '/media'
     file_overwrite = True
