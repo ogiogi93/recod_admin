170d991e7d450d82c23e442226514d68369f6d73
ogiogi93 <mail_ogiwara93@dx5.so-net.ne.jp>
2018-03-05 10:24:42 +0900

    staticファイルの設定変更

--- recod_admin/settings_production.py
+++ recod_admin/settings_production.py
@@ -1,6 +1,8 @@
 from .settings import *
 
 ENV = 'production'
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+DEBUG = False
+
+STATIC_ROOT = '/var/www/recod_admin/staticfiles/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'recod_admin.storage_backends.MediaStorage'
