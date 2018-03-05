cd8053bb8418b60fced410960d34355c3475b39f
ogiogi93 <mail_ogiwara93@dx5.so-net.ne.jp>
2018-03-05 12:07:59 +0900

    fix

--- recod_admin/settings_production.py
+++ recod_admin/settings_production.py
@@ -4,5 +4,4 @@ ENV = 'production'
 DEBUG = False
 
 STATIC_ROOT = '/var/www/recod_admin/staticfiles/'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'recod_admin.storage_backends.MediaStorage'
